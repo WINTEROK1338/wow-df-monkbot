075874fbad2a2252d287670bd4bed66631ae05a8
WINTEROK1338 <60034572+WINTEROK1338@users.noreply.github.com>
2023-06-26 11:47:27 +0300

    Add files via upload

---
+++ myy.py
@@ -0,0 +1,24 @@
+import pyautogui
+import time
+import random
+
+while True:
+    pyautogui.press('4')
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.press('3')
+    time.sleep(1)
+    pyautogui.press('7')
+    time.sleep(1)
+    pyautogui.press('5')
+    time.sleep(1)
+    pyautogui.press('7')
+    time.sleep(1)
+    pyautogui.press('6')
+    time.sleep(1)
+    pyautogui.press('7')
+    time.sleep(1)
+    pyautogui.press('8')
+    time.sleep(1)
+    pyautogui.click()
+    time.sleep(1)
